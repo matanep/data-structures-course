c532ffb5c841c3708c304e4dd8dde82ef529f001
Matan <matanep@gmail.com>
2017-04-16 14:32:28 +0300

    q_3

--- Homework assignment 1/q_3.py
+++ Homework assignment 1/q_3.py
@@ -9,25 +9,32 @@ path="data3.xlsx"
 
 #Implement HashTable class:
 class HashTable:
-    def __init__(self, size=int , hash_function_method=str, collision_handling=str,m=int,A=float,m_2=0,A_2=0.0):
+    def __init__(self, size=int , hash_function_method=str, collision_handling=str,m=int,A=float,m_2=int,A_2=float): #Initiate all the parameter for the hash table.
         self.hash_function_method=hash_function_method
         self.collision_handling=collision_handling
         self.size = size
-        self.keys =[[] for _ in range(size)]
-        self.data = [[] for _ in range(size)]
+        self.keys =[[] for _ in range(size)] #Data structure for keys.
+        self.data = [[] for _ in range(size)]#Data structure for values.
         self.m=m
         self.A=A
         self.m_2=m_2
         self.A_2=A_2
         self.num_keys=0
+        #Check that all the parameters are given for a specific configuration of the hash table:
+        if self.hash_function_method=="multiplication" and self.A ==float:
+            raise ValueError('Need to define A')
+        if self.collision_handling=="OA_Double_Hashing" and self.m_2 ==int:
+            raise ValueError('Need to define m_2')
+        if self.collision_handling=="OA_Double_Hashing" and self.hash_function_method=="multiplication" and self.A_2 ==float:
+            raise ValueError('Need to define A_2')
 
-    def hash_function(self,key):
+    def hash_function(self,key):                #Logic of hash function.
         if self.hash_function_method=="mod":
             return key%self.m
         elif self.hash_function_method=="multiplication":
             return int(self.m*(key*self.A-int(key*self.A)))
 
-    def hash_function_2(self,key):
+    def hash_function_2(self,key):              #Logic of hash function, when using OA_Double_Hashing.
         if self.hash_function_method=="mod":
             return (self.m_2-key%self.m_2)
         elif self.hash_function_method=="multiplication":
@@ -35,79 +42,79 @@ class HashTable:
 
 
     def insert(self,key=int,value=str):
-        counter=0
+        counter=0                           #We will use the counter later to create our metric of efficiency.
         place= self.hash_function(key)
-        if self.keys[place]==[]:
+        if self.keys[place]==[]:            #If we did not have any collision.
             self.keys[place]=[key]
             self.data[place]=[data]
             counter+=1
             self.num_keys += 1
             return counter
-        elif self.keys[place]==[key]:
+        elif self.keys[place]==[key]:       #Replacing the value.
             self.data[place]=[data]
             counter+=1
             return counter
-
+        #hendeling collisions:
         elif self.collision_handling=="Chain":
             if len(self.keys[place])>=1:
                 counter+=1
                 for i in range(1,len(self.keys[place])):
                     counter+=1
-                    if self.keys[place][i]==key:
+                    if self.keys[place][i]==key:        #Replacing the value.
                         self.data[place][i]=value
                         self.num_keys += 1
-                        return counter
-                self.keys[place].append(key)
+                        return counter                  #Returns the amount operations.
+                self.keys[place].append(key)            #Using 'Chain'.
                 self.data[place].append(value)
                 self.num_keys += 1
-                return counter
+                return counter                          #Returns the amount operations.
 
         elif self.collision_handling=="OA_Quadratic_Probing":
             if self.num_keys==self.size:
-                return "Hash Table is full"
+                return "Hash Table is full"             #In open addressing, we can not have more keys then the size of the data structure.
             counter+=1
             for i in range(1,self.size):
                 counter+=1
                 new_place=self.hash_function(place +i*i)
-                if self.keys[new_place]==[key]:
+                if self.keys[new_place]==[key]:         #Replacing the value.
                     self.data[new_place]=[value]
-                    return counter
+                    return counter                      #Returns the amount operations.
                 elif self.keys[new_place]==[]:
-                    self.keys[new_place]=[key]
+                    self.keys[new_place]=[key]          #Using 'OA_Quadratic_Probing'.
                     self.data[new_place]=[value]
                     self.num_keys+=1
-                    return counter
+                    return counter                      #Returns the amount operations.
 
         elif self.collision_handling=="OA_Double_Hashing":
             if self.num_keys==self.size:
-                return "Hash Table is full"
+                return "Hash Table is full"             #In open addressing, we can not have more keys then the size of the data structure.
             counter+=1
             for i in range(1,self.size):
                 counter+=1
                 new_place=self.hash_function(key+i*self.hash_function_2(key))
-                if self.keys[new_place] == [key]:
+                if self.keys[new_place] == [key]:       #Replacing the value.
                     self.data[new_place] = [value]
-                    return counter
+                    return counter                      #Returns the amount operations.
                 elif self.keys[new_place]==[]:
                     self.keys[new_place]=[key]
-                    self.data[new_place]=[value]
+                    self.data[new_place]=[value]        #Using 'OA_Double_Hashing'.
                     self.num_keys+=1
-                    return counter
+                    return counter                      #Returns the amount operations.
 
     def delete(self,key=int):
-        counter = 0
+        counter = 0                                     #We will use the counter later to create our metric of efficiency.
         place = self.hash_function(key)
         if self.keys[place] == [key]:
             del self.keys[place]
             del self.data[place]
-            counter += 1
+            counter += 1                                #If we did not have any collision.
             self.num_keys -= 1
-            return counter
+            return counter                              #Returns the amount operations.
 
         elif self.collision_handling == "Chain":
             if self.keys[place] == []:
                 counter += 1
-                return "Data is not in Hash Table",counter
+                return "Data is not in Hash Table",counter   #
             if len(self.keys[place]) >= 1:
                 counter+=1
                 for i in range(1,len(self.keys[place])):
@@ -208,15 +215,13 @@ for data,keys in datas_and_keys: #Go over each data set.
     Hash_Table_5=HashTable(149,"multiplication","OA_Quadratic_Probing",149,0.589,0,0)
     Hash_Table_6=HashTable(149,"multiplication","OA_Double_Hashing",149,0.589,97,0.405)
     Hash_Tables=[Hash_Table_1,Hash_Table_2,Hash_Table_3,Hash_Table_4,Hash_Table_5,Hash_Table_6]
+    hn=1
     for Hash_Table in Hash_Tables:
         count_in=0
         count_is=0
         for i in range(len(keys)):
             moves=Hash_Table.insert(int(keys[i]),value=data[i])
             count_in+=moves
-        print float(count_in)/len(keys)
+        print '##Hash Table '+str(hn)+':',float(count_in)/len(keys)
+        hn+=1
     n+=1
-
-
-
-#Todo: raise error if input parameters are not defined
