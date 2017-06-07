e36e3afac179c865eb8c1d3248416f30c0dacde8
Matan <matanep@gmail.com>
2017-06-07 11:49:59 +0300

    home 2 +data

--- Homework assignment 2/q_2.py
+++ Homework assignment 2/q_2.py
@@ -12,7 +12,19 @@ data = pd.ExcelFile(path)
 df = data.parse("Sheet1")
 edges=[tuple(x) for x in df.to_records(index=False)]
 
-number_of_vertices= max(max(edges))
+
+# for i in edges:
+#     edges.remove((i[1],i[0]))
+#
+#
+# # print len(edges)
+# df=pd.DataFrame.from_records(edges)
+#
+# # print df
+# #
+# writer = pd.ExcelWriter('edges.xlsx')
+# df.to_excel(writer,'Sheet2')
+# writer.save()
 
 #part a
 def creat_adjacency_matrix(edges=list):
@@ -23,6 +35,7 @@ def creat_adjacency_matrix(edges=list):
         matrix[edge[0]-1,edge[1]-1]=1
         matrix[edge[1]-1, edge[0]-1] = 1
     return matrix
+
 #part b
 def creat_adjacency_dict(edges=list):
     adjacency_dict={}
@@ -38,24 +51,48 @@ def creat_adjacency_dict(edges=list):
             adjacency_dict[edge[1]]=[edge[0]]
     return adjacency_dict
 
-print creat_adjacency_dict(edges)
+print creat_adjacency_dict(edges)[1]
 
 
 #part c
+class Queue():
+    def __init__(self,max_size):
+        self.max_size= max_size
+        self.items = []
+
+    def front(self):
+        return self.items[-1]
+
+    def empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        if len(self.items)<self.max_size:        #Check that the length of the queue is less than the 'maximum_size'.
+            self.items.insert(0,item)
+        else:
+            return "Queue is full"
+
+    def dequeue(self):
+        return self.items.pop()
 
 def BFS(edges=list,v=int):
     number_of_vertices = max(max(edges))
+    queue=Queue(number_of_vertices)
     visited=[False for i in range(number_of_vertices)]
     print(v)
-    visited[v]= True
-    ENQUEUE(v,Q)
-    while (not EMPTY(Q)):
-        x  DEQUEUE(Q)
-        for every neighbor y of x  {
-                     if (not visited [y])  {
-                           print(y)
-                           visited[y]  TRUE
-                           ENQUEUE(y,Q)
+    visited[v-1]= True
+    queue.enqueue(v)
+    adjacency_dict=creat_adjacency_dict(edges)
+    while (not queue.empty()):
+        x= queue.dequeue()
+        neighbors_of_x=adjacency_dict[x]
+        for neighbor in neighbors_of_x:
+            print neighbor
+            if not visited[neighbor-1]:
+                print neighbor
+                visited[neighbor-1]=True
+                queue.enqueue(neighbor)
 
+print BFS(edges,1)
 
 #part d
